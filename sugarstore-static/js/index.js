3c165661ff5656a1b4fb83095a1a5f35fe9de527
Srevin Saju <srevin03@gmail.com>
2020-06-08 23:04:02 +0300

    :tada: Initial commit

---
+++ sugarstore-static/js/index.js
@@ -0,0 +1,75 @@
+var miniSearch
+
+function clearActivityCards() {
+    $('#activity-card-column').empty();
+}
+
+function getActivityIndex () {
+  
+}
+
+function addActivityCard(item) {
+    var name = item['name'];
+    var icon_path = item['icon_name'];
+    var summary = item['summary'] != null ? item['summary'] : 'No info provided';
+    var url_container
+    if ($.trim( item['url']) != ""){
+        url_container = `<a href="${item['url']}" class="btn btn-primary"><i class="fa fa-info-circle"></i></a>`
+    } else {
+        url_container = ""
+    }
+    var bundle_path = `../bundles/${item['bundle_name']}`
+    
+    $('#activity-card-column').append(
+        `<div class="card saas-card">\
+            <img class="card-img-top" style="padding:12%" src="../icons/${icon_path}.svg" alt="${name} Icon">\
+            <div class="card-body">\
+                <h5 class="card-title">${name}</h5>\
+                <p class="card-text">${summary}</p>\
+                <a href="${bundle_path}" class="btn btn-primary"><i class="fa fa-download"></i></a>\
+                ${url_container}
+            </div>\
+        </div>`
+    )
+}
+
+
+function loadAllActivities () {
+    // get the json file
+    if ($.trim( $('#saas-search-box').val() ) != ''){
+        // the user has entered something, filter the list accordingly
+        $.getJSON("../index.json", function(data) {
+            console.log("Searching using miniSearch");
+            if (miniSearch == null){
+                // index minisearch once and only once
+                // reduces CPU usage
+
+                console.log("minisearch indexed.")
+                miniSearch = new MiniSearch({
+                    fields: ['name', 'summary'], // fields to index for full-text search
+                    storeFields: ['name', 'summary', 'url', 'icon_name', 'bundle_name'], // fields to return with search results
+                    searchOptions: {
+                        boost: { title: 2 },
+                        fuzzy: 0.5
+                    }
+                });
+                // Index all documents
+                miniSearch.addAll(data);
+            }
+            let results = miniSearch.search($('#saas-search-box').val())
+            $.each(results, function(i, item){
+                addActivityCard(item)
+            })
+        });
+    } else {
+        $.getJSON("../index.json", function(data) {
+        console.log(data)
+        // update the UI with each card
+        $.each(data, function(i, item){
+            addActivityCard(item)
+        })
+        }); 
+    }
+    
+
+};
