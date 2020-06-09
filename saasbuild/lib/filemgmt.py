97516fe2fa69779a11e53e1e8f6d91e07bc38926
Srevin Saju <srevin03@gmail.com>
2020-06-09 15:24:24 +0300

    Add utils and platform support

---
+++ saasbuild/lib/filemgmt.py
@@ -0,0 +1,29 @@
+"""
+Sugar Activities App Store (SAAS)
+https://github.com/sugarlabs-aslo/sugarappstore
+
+Copyright 2020 SugarLabs
+Copyright 2020 Srevin Saju <srevinsaju@sugarlabs.org>
+Copyright 2020 Manish <sugar@radii.dev>
+
+This file is part of "Sugar Activities App Store" aka "SAAS".
+
+SAAS is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SAAS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with SAAS.  If not, see <https://www.gnu.org/licenses/>.
+"""
+#!/usr/bin/python
+import os
+import sys
+
+
+FILE_PROTOCOL = "file://"
