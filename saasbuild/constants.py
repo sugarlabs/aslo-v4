7a5ffa6ecb58dea6485a2e7e60ca6243cead3235
Srevin Saju <srevin03@gmail.com>
2020-06-15 15:53:26 +0300

    Missing < tag on lastmode sitemap.xml

--- saasbuild/constants.py
+++ saasbuild/constants.py
@@ -84,18 +84,16 @@ HTML_TEMPLATE = """
 """
 
 SITEMAP_HEADER = \
-"""
-<?xml version="1.0" encoding="UTF-8"?>
+"""<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 {content}
 </urlset>
 """
 
 SITEMAP_URL = \
-"""
-<url>
+"""<url>
   <loc>{url}</loc>
-  <lastmod>{lastmod}/lastmod>
+  <lastmod>{lastmod}</lastmod>
   <changefreq>{changefreq}</changefreq>
   <priority>0.8</priority>
 </url>
