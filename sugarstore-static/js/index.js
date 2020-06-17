78464a40cd8f111abdcc54c57ebd1cb956e732a2
Srevin Saju <srevin03@gmail.com>
2020-06-17 12:28:45 +0300

    Use 2 spaces instead of tab

--- sugarstore-static/js/index.js
+++ sugarstore-static/js/index.js
@@ -28,31 +28,31 @@ function getCookie(cname) {
 
 
 function checkCookiesEnabledShowConsent(){
-    /* Check if cookie consent has been accepted,
-        if not, request the user for a consent
-    */
-    if (getCookie("saas-fun") == "" && getCookie("saas-cookie-consent-accepted") == ""
-        ) {
-        console.log("Cookie consent has not been accepted")
-        // means cookie is not set yet!
+  /* Check if cookie consent has been accepted,
+      if not, request the user for a consent
+  */
+  if (getCookie("saas-fun") == "" && getCookie("saas-cookie-consent-accepted") == ""
+      ) {
+      console.log("Cookie consent has not been accepted")
+      // means cookie is not set yet!
 
-        // add onclick event listener to setCookie cookie accepted
-        $('#cookie-consent-accept-btn').click(function(evt) {
-          setCookie("saas-cookie-consent-accepted", "true", 30)
-        });
+      // add onclick event listener to setCookie cookie accepted
+      $('#cookie-consent-accept-btn').click(function(evt) {
+        setCookie("saas-cookie-consent-accepted", "true", 30)
+      });
 
-        // ask the user for permission
-        $('#cookieModal').modal('show');
-        return false
-    } else {
-        console.log("Cookie consent has already been accepted.")
-        return true
-    }
+      // ask the user for permission
+      $('#cookieModal').modal('show');
+      return false
+  } else {
+      console.log("Cookie consent has already been accepted.")
+      return true
+  }
 }
 
 function restoreConfigurationOnLoad() {
-    /* Restores the configuration as set in cookies */
-    // check if a cookie is set to true
+  /* Restores the configuration as set in cookies */
+  // check if a cookie is set to true
     if ( getCookie("saas-fun") == "true" ) {
       // restore the saved cookie
       console.log("Restoring configuration from cookie")
@@ -68,13 +68,15 @@ function restoreConfigurationOnLoad() {
 }
 
 function enableFunGradientBackground(){
-    console.log("Enabling fun gradient background animation.");
-    $("body").addClass("fun-gradient-animation-bg");
-    setCookie("saas-fun", "true", 365);
+  console.log("Enabling fun gradient background animation.");
+  $("body").addClass("fun-gradient-animation-bg");
+  setCookie("saas-fun", "true", 365);
 }
 
 function disableFunGradientBackground() {
-    console.log("Disabling fun gradient background animation");
-    $("body").removeClass("fun-gradient-animation-bg");
-    setCookie("saas-fun", "false", 365);
+  console.log("Disabling fun gradient background animation");
+  $("body").removeClass("fun-gradient-animation-bg");
+  setCookie("saas-fun", "false", 365);
+}
+
 }
