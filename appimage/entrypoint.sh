9aa37644943a06516dc9859621e9c842f2b06a6f
Srevin Saju <srevinsaju@sugarlabs.org>
2020-07-02 15:48:46 +0300

    Use global packages for appimage

--- appimage/entrypoint.sh
+++ appimage/entrypoint.sh
@@ -1,4 +1,4 @@
 export LD_LIBRARY_PATH="${APPDIR}/usr/lib:${LD_LIBRARY_PATH}"
 export PATH="${APPDIR}/usr/bin:${PATH}"
 
-{{ python-executable }} -s ${APPDIR}/usr/bin/saasbuild "$@"
+{{ python-executable }} ${APPDIR}/usr/bin/saasbuild "$@"
