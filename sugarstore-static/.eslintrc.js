ae0274dfc0b3b3540b71ffc1634482e048425004
Srevin Saju <srevinsaju@sugarlabs.org>
2020-07-04 15:03:35 +0300

    Add .eslintrc

---
+++ sugarstore-static/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2020': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 11,
+  },
+  'rules': {
+  },
+};
