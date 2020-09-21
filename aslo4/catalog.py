8c4c331f9069141257cfb3ee53d190f1c80cb9d7
Srevin Saju <srevinsaju@sugarlabs.org>
2020-09-21 08:51:46 +0000

    feat: fix title and better links to Bug tracker
    May fix #55
    
    Using catalog object, rewrite parts of the static html so that
    the same data gets reused

--- aslo4/catalog.py
+++ aslo4/catalog.py
@@ -3,3 +3,24 @@ class Catalog:
         self.name = "Sugar App Store"
         self.prefix = "/aslo"
         self.description = "Sugar Labs App Store"
+        self.organization = "Sugar Labs"
+        self.search_box = {
+            "placeholder_text": "Search for Activities!"
+        }
+        self.org_details = {
+            "homepage": "https://sugarlabs.org",
+            "wiki": "https://wiki.sugarlabs.org",
+            "legacy_appstore": "https://activities.sugarlabs.org/",
+        }
+        self.git_repository = "https://github.com/sugarlabs/aslo-v4"
+        self.is_github = True
+
+    @property
+    def bug_tracker(self):
+        if self.is_github:
+            return "{}/issues".format(self.git_repository)
+        else:
+            return ""
+
+
+
